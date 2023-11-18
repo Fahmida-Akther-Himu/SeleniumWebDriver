164474d295fcb63494830a857a1ad356edf7e21a
fahmida.akther <fahmida.akther@bracits.com>
2023-11-18 14:07:13 +0600

    selenium second assignment commit

---
+++ src/test/java/com/selenium/secondAssignment/WebElementTest.java
@@ -0,0 +1,50 @@
+package com.selenium.secondAssignment;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class WebElementTest {
+    public static void main(String[] args) throws InterruptedException {
+        //Browser Launch
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://pilotprocurement.brac.net/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        //Interacting with 5 web elements
+        WebElement username = driver.findElement(By.cssSelector("input[id$='username_txt']"));      //Dynamic selectors - * contains
+        username.clear();       //clear - only applies to text fields and content editable elements
+        username.sendKeys("ahsan.habib");       //send keys - only applies to text fields and content editable elements
+
+        WebElement password = driver.findElement(By.cssSelector("input[type^='password']"));        //Dynamic selectors - ^ start with
+        password.clear();
+        password.sendKeys("abc123$");
+
+        //driver.findElement(By.cssSelector("button[type$='button']")).click();       //Dynamic selectors - $ end and click - applies to any element
+        driver.findElement(By.cssSelector("form[id='loginForm']")).submit();        //submit use only applies to form elements
+        //driver.findElement(By.id("loginForm")).submit();
+
+        //Information about web elements
+        //Print boolean value for .isDisplayed(), .isEnabled(), .isSelected()
+        System.out.println(username.isDisplayed());     //Print the Element is displayed on a webpage by using .isDisplayed()
+        System.out.println(username.isEnabled());       //Print the Element is enabled or disabled on a webpage by using .isEnabled()
+        System.out.println(username.isSelected());      //Print the referenced Element is Selected or not by using .isSelected()
+        System.out.println(username.getTagName());      //Print the tag name by using .getTagName()
+        System.out.println("Print x axis: " + username.getRect().x + "\n" + "Print y axis: " + username.getRect().y);      //Print the dimensions and coordinates by using .getRect()
+        System.out.println(username.getCssValue("font-size").trim());       //Print the value of specified computed style property by using .getCssValue("color")
+        System.out.println(driver.findElement(By.cssSelector(".copyright")).getText().trim());      //Print the Text Content by using .getText()
+        System.out.println(driver.findElement(By.cssSelector("img[class$='img-responsive center']")).getAttribute("src"));      //Print the URL by using .getAttribute()
+
+        driver.get("https://the-internet.herokuapp.com/upload&quot");
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
